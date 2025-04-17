bf9b304d665bb4b10edbef5c7adc8ebe76727b7f
Lan Jiang <jianglan1350@gmail.com>
2025-04-17 09:55:52 +0200

    fix bug

--- tests/database/test_db_config.py
+++ tests/database/test_db_config.py
@@ -8,7 +8,7 @@ class TestDBConfig(unittest.TestCase):
     def test_get_db_config(self):
         with get_db_connection() as db_connection:
             self.assertIsNotNone(db_connection)
-            self.assertEqual(db_connection.info.dbname, 'bgg')
-            self.assertEqual(db_connection.info.user, 'ljiang')
+            self.assertEqual(db_connection.info.dbname, 'testdb')
+            self.assertEqual(db_connection.info.user, 'postgres')
             self.assertEqual(db_connection.info.host, 'localhost')
             self.assertEqual(db_connection.info.port, 5432)
